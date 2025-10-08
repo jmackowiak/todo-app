c8e12a527c9814cbe670505370b6bebdf358c92a
jmackowiak <jmackowak@gmail.com>
2025-10-08 23:30:47 +0200

    add image component and button

---
+++ components/ImageViewer.tsx
@@ -0,0 +1,17 @@
+import { Image, ImageSourcePropType, StyleSheet } from 'react-native'
+
+type ImageViewerProps = {
+  imgSource: ImageSourcePropType
+}
+
+export default function ImageViewer({ imgSource }: ImageViewerProps) {
+  return <Image source={imgSource} style={styles.image} />
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+})
