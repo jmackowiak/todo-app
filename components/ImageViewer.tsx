b7723929e3e3f6a85686a213940ba089d2dbed71
jmackowiak <jmackowak@gmail.com>
2025-12-13 20:07:58 +0100

    add biome

--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,23 +1,23 @@
-import { Image, ImageSourcePropType, StyleSheet } from 'react-native'
+import { Image, type ImageSourcePropType, StyleSheet } from 'react-native'
 
 type ImageViewerProps = {
-  imgSource: ImageSourcePropType
-  selectedImage?: string
+	imgSource: ImageSourcePropType
+	selectedImage?: string
 }
 
 export default function ImageViewer({
-  imgSource,
-  selectedImage,
+	imgSource,
+	selectedImage,
 }: ImageViewerProps) {
-  const source = selectedImage ? { uri: selectedImage } : imgSource
+	const source = selectedImage ? { uri: selectedImage } : imgSource
 
-  return <Image source={source} style={styles.image} />
+	return <Image source={source} style={styles.image} />
 }
 
 const styles = StyleSheet.create({
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
+	image: {
+		width: 320,
+		height: 440,
+		borderRadius: 18,
+	},
 })
