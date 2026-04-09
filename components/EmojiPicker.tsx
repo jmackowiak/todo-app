a8130498dc4dd0a8cca4a5d1bcdb805268c46f0e
jmackowiak <jmackowak@gmail.com>
2026-04-09 23:14:26 +0200

    fix props order

--- components/EmojiPicker.tsx
+++ components/EmojiPicker.tsx
@@ -1,55 +1,55 @@
 import { MaterialIcons } from '@expo/vector-icons'
-import { PropsWithChildren } from 'react'
+import type { PropsWithChildren } from 'react'
 import { Modal, Pressable, StyleSheet, Text, View } from 'react-native'
 
 type EmojiPickerProps = PropsWithChildren<{
-  isVisible: boolean
-  onClose: () => void
+	isVisible: boolean
+	onClose: () => void
 }>
 
 export default function EmojiPicker({
-  isVisible,
-  onClose,
-  children,
+	isVisible,
+	onClose,
+	children,
 }: EmojiPickerProps) {
-  return (
-    <View>
-      <Modal animationType="slide" transparent visible={isVisible}>
-        <View style={styles.modalContent}>
-          <View style={styles.titleContainer}>
-            <Text style={styles.title}>Choose a sticker</Text>
-            <Pressable onPress={onClose}>
-              <MaterialIcons name="close" color="#fff" size={22} />
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-    </View>
-  )
+	return (
+		<View>
+			<Modal animationType="slide" transparent visible={isVisible}>
+				<View style={styles.modalContent}>
+					<View style={styles.titleContainer}>
+						<Text style={styles.title}>Choose a sticker</Text>
+						<Pressable onPress={onClose}>
+							<MaterialIcons color="#fff" name="close" size={22} />
+						</Pressable>
+					</View>
+				</View>
+			</Modal>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
-  modalContent: {
-    height: '25%',
-    width: '100%',
-    backgroundColor: '#25292e',
-    borderTopRightRadius: 18,
-    borderTopLeftRadius: 18,
-    position: 'absolute',
-    bottom: 0,
-  },
-  titleContainer: {
-    height: '16%',
-    backgroundColor: '#464C55',
-    borderTopRightRadius: 10,
-    borderTopLeftRadius: 10,
-    paddingHorizontal: 20,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 16,
-  },
+	modalContent: {
+		height: '25%',
+		width: '100%',
+		backgroundColor: '#25292e',
+		borderTopRightRadius: 18,
+		borderTopLeftRadius: 18,
+		position: 'absolute',
+		bottom: 0,
+	},
+	titleContainer: {
+		height: '16%',
+		backgroundColor: '#464C55',
+		borderTopRightRadius: 10,
+		borderTopLeftRadius: 10,
+		paddingHorizontal: 20,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+	title: {
+		color: '#fff',
+		fontSize: 16,
+	},
 })
--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -2,27 +2,27 @@ import MaterialIcons from '@expo/vector-icons/MaterialIcons'
 import { Pressable, StyleSheet, Text } from 'react-native'
 
 type IconButtonProps = {
-  onPress: () => void
-  icon: keyof typeof MaterialIcons.glyphMap
-  label: string
+	onPress: () => void
+	icon: keyof typeof MaterialIcons.glyphMap
+	label: string
 }
 
 export default function IconButton({ onPress, icon, label }: IconButtonProps) {
-  return (
-    <Pressable style={styles.iconButton} onPress={onPress}>
-      <MaterialIcons name={icon} size={24} color="#fff" />
-      <Text style={styles.iconButtonLabel}>{label}</Text>
-    </Pressable>
-  )
+	return (
+		<Pressable onPress={onPress} style={styles.iconButton}>
+			<MaterialIcons color="#fff" name={icon} size={24} />
+			<Text style={styles.iconButtonLabel}>{label}</Text>
+		</Pressable>
+	)
 }
 
 const styles = StyleSheet.create({
-  iconButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  iconButtonLabel: {
-    color: '#fff',
-    marginTop: 12,
-  },
+	iconButton: {
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	iconButtonLabel: {
+		color: '#fff',
+		marginTop: 12,
+	},
 })
